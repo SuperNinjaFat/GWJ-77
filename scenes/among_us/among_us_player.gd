a7593da467cd1195e9fb59586c0d2f066ed5a5cc
Super Ninja Fat <paulmlindberg@gmail.com>
2025-01-11 18:17:23 -0500

    Convert 2D AmongUs to 3D

--- scenes/among_us/among_us_player.gd
+++ scenes/among_us/among_us_player.gd
@@ -1,32 +1,32 @@
-extends CharacterBody2D
+extends CharacterBody3D
 
-const SPEED = 250
+const SPEED = 7.5
 
 func _physics_process(delta):
     # animate
     if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down"):
-        get_node("AnimatedSprite2D").play("move")
+        get_node("AnimatedSprite3D").play("move")
     else:
-        get_node("AnimatedSprite2D").play("idle")
+        get_node("AnimatedSprite3D").play("idle")
 
     # apply velocity with movement
     if Input.is_action_pressed("ui_right"):
         velocity.x = SPEED
-        get_node("AnimatedSprite2D").flip_h = false
+        get_node("AnimatedSprite3D").flip_h = false
     elif Input.is_action_pressed("ui_left"):
         velocity.x = -SPEED
-        get_node("AnimatedSprite2D").flip_h = true
+        get_node("AnimatedSprite3D").flip_h = true
     else:
         velocity.x = 0
     
-    # velocity.y = velocity.y
+    # velocity.z = velocity.z
 
     if Input.is_action_pressed("ui_up"):
-        velocity.y = -SPEED
+        velocity.z = -SPEED
     elif Input.is_action_pressed("ui_down"):
-        velocity.y = SPEED
+        velocity.z = SPEED
     else:
-        velocity.y = 0
+        velocity.z = 0
     ## Godot 3:
     #velocity = move_and_slide(velocity, Vector2.UP)
     move_and_slide()
