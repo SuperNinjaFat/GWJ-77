ba45fc261b548948290579137a7dfef9f5f9a07f
Super Ninja Fat <paulmlindberg@gmail.com>
2025-02-15 14:22:00 -0500

    Add interaction manager area registering progress
    Expose interaction area registering for the player entering the areas (Note that this was broken in the old interaction system)

--- scenes/overworld/interaction/interaction_area.gd
+++ scenes/overworld/interaction/interaction_area.gd
@@ -6,3 +6,19 @@ var interact: Callable = func(_params):
 	pass
 
 var enabled: bool = false
+
+func is_player_leave_or_exit(body: Node3D):
+	return !enabled or !body.is_in_group("player")
+
+func _on_body_entered(body: Node3D) -> void:
+	if is_player_leave_or_exit(body):
+		return
+	# print("Player entered ", self, ": ", body)
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if is_player_leave_or_exit(body):
+		return
+	# print("Player left ", self, ": ", body)
+	InteractionManager.unregister_area(self)
--- scenes/overworld/interaction/interaction_manager.gd
+++ scenes/overworld/interaction/interaction_manager.gd
@@ -4,7 +4,16 @@ extends Node
 ## Enabled interaction areas the overworld player is near enough to interact with
 var active_areas = []
 
+func register_area(area: InteractionArea):
+	active_areas.push_back(area)
+
+func unregister_area(area: InteractionArea):
+	var index = active_areas.find(area)
+	if index == -1:
+		push_error("Couldn't unregister area ", area.name)
+		return
+	active_areas.remove_at(index)
+
 # TODO:
-## Expose adding and removing an interaction area to active_areas for when the overworld player leaves them.
 ## Handle on-frame interaction: Hiding/unhiding input labels based on active_areas (determine priority by sorting them here, too)
 ## Handle pressing interactions (input delay, _input pressing interaction)
